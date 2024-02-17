71a356a30a0fa1bae74d50e1235d41badf2590a0
Josh Carlson <837837+magneticstain@users.noreply.github.com>
2024-02-17 14:55:28 -0500

    Add mfa admin jump role

--- modules/bootstrap_account/main.tf
+++ modules/bootstrap_account/main.tf
@@ -32,3 +32,54 @@ resource "aws_iam_group_membership" "admin-user-grp-membership" {
   group = aws_iam_group.admin-group.name
   users = [aws_iam_user.admin-user.name]
 }
+
+## MFA Admin Role
+### Policy
+resource "aws_iam_policy" "mfa-admin-role-policy" {
+  name_prefix = "mfa-admin-role-policy-"
+
+  description = "IAM policy used for allowing users to assume a dedicated role for further role linking."
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowAssumeRole",
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+  EOF
+}
+
+### Role
+resource "aws_iam_role" "mfa-admin-role" {
+  name = "mfa-admin-jump"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "XaccountAccessPrimaryUser",
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::509915386432:user/josh"
+      },
+      "Condition": {
+        "Bool": {
+          "aws:multifactorAuthPresent": "true"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "mfa-admin-role-policy-attach" {
+  role       = aws_iam_role.mfa-admin-role.name
+  policy_arn = aws_iam_policy.mfa-admin-role-policy.arn
+}
