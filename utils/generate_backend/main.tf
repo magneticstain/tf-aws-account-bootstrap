6d871d4f08ead42f27261fc25ae19d03e582062c
Josh Carlson <837837+magneticstain@users.noreply.github.com>
2024-02-20 19:52:23 -0500

    Update tf backend plan to match project

--- utils/generate_backend/main.tf
+++ utils/generate_backend/main.tf
@@ -1,14 +1,10 @@
-resource "aws_s3_bucket" "tf_backend-ip2cr" {
-  bucket_prefix = "tf-ip2cr-"
+resource "aws_s3_bucket" "tf_backend" {
+  bucket_prefix = "tf-aws-account-bootstrap-child-"
   force_destroy = true
-
-  tags = {
-    Name  = "IP2CR Terraform state bucket"
-  }
 }
 
-resource "aws_dynamodb_table" "tf_backend-ip2cr" {
-  name            = "tf-ip2cr"
+resource "aws_dynamodb_table" "tf_backend" {
+  name            = "tf-aws-account-bootstrap-child"
   hash_key        = "LockID"
   billing_mode    = "PROVISIONED"
   read_capacity   = 5
@@ -20,9 +16,9 @@ resource "aws_dynamodb_table" "tf_backend-ip2cr" {
   }
 }
 
-output "tf-ip2cr-metadata" {
+output "tf-backend-metadata" {
   value = [
-    resource.aws_s3_bucket.tf_backend-ip2cr.bucket,
-    resource.aws_dynamodb_table.tf_backend-ip2cr.name
+    resource.aws_s3_bucket.tf_backend.bucket,
+    resource.aws_dynamodb_table.tf_backend.name
   ]
 }
